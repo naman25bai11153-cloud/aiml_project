86f447554f9c4331da8d44d999b8c0785dc1cdc8
Naman Singh <naman.25bai11153@vitbhopal.ac.in>
2026-03-31 01:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
 import geopy
 import geocoder
-
+from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="Emergency_App")
 def nearby_hospital():
     city = input("Enter your city: ")
-    loc = input("enter ")
+    loc = input("enter your current location: ")
+    address = f"{loc}, {city}"
+    location = geolocator.geocode(address)
+    if location:
+        print(f"Coordinates Found: {location.latitude}, {location.longitude}")
+        return location.latitude, location.longitude, city
+    else:
+        print("Could not find that specific area. Try a nearby landmark.")
+        return None
+#    url = "https://overpass-api.de/api/interpreter"
+
+nearby_hospital()
