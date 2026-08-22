fee68a7190ca30f3525afa3ddbfad9ed174b38b3
Naman Singh <naman.25bai11153@vitbhopal.ac.in>
2026-08-23 00:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,14 @@
 import requests
 from geopy.geocoders import Nominatim
-aaa = Nominatim(user_agent="emergency hospital route finder using bfs")
+from location_tracker import hsl
+from hospital_finder import hospital_loc
+from distance_calculator import hkv
 # finding coordinates of your location
-def hsl():
-    c = input("Enter your city name : ")
-    a = input("enter your current location as precisely as you can : ")
-    add = f"{a}, {c}"
-    loc = aaa.geocode(add)
-    if loc:
-        lat = loc.latitude
-        lon = loc.longitude
-        return lat, lon, c
-    else:
-        print("Could not find that area. "
-              "Try a more known landmark/place .")
-        return None
+
 #now we will find nearby hospitals
-def hospital_loc(lat,lon):
-    print("finding hospitals nearby..")
-    #now we will use OSM api to find nearby hospitals near you. OSM is detailed map that provides accurate coordinates
-    url = "https://overpass.kumi.systems/api/interpreter"
-    query = (f'[out:json];'
-             f'node["amenity"="hospital"](around:5000,{lat},{lon});'
-             f'out body;')
-    headers = {'User-Agent': 'Emergency_route_finder/1.0'}
-    try:
-        response = requests.post(url, data={'data': query}, headers=headers)
 
-        if response.status_code != 200:
-            print(f"Server returned an error: {response.status_code}")
-            return []
-        d= response.json()
-        hos = []
-
-        for item in d.get('elements', []):
-            name = item.get('tags', {}).get('name', 'General Hospital')
-            hos.append({'name': name,
-                'lat': item['lat'],
-                'lon': item['lon']})
-
-        if hos:
-            print("nearby hospitals are :")
-            for h in hos[:10]:
-                print(f"> {h['name']} : Lat: {h['lat']}, Lon: {h['lon']}")
-            return hos #returns the list of hospitals
-
-
-        else:
-            print("No hospitals found in a 5km radius.")
-
-    except Exception as e:
-        print(f"Server Error: {e}")
-        return []
-
-
-def hkv(clat, clon, hlat, hlon):
-    # OSRM Public API URL to find the road distance between current location and nearby hospitals.
-    # OSRM calculates the disntances from one coordinate to onther on a map
-
-    url = f"http://router.project-osrm.org/route/v1/driving/{clon},{clat};{hlon},{hlat}?overview=false"
-
-    try:
-        # adding a small timeout so the program doesn't hang if the server is slow
-        response = requests.get(url, timeout=5)
-        data = response.json()
-
-        if data['code'] == 'Ok':
-            dis = data['routes'][0]['distance']
-            # converting distance from meters to killometers
-            dis = dis/1000
-            dis = round(dis, 2)
-            return dis
-    except Exception:
-        return None
-    return None
 def main():
-    c = hsl() #getting your cordinates
+    c = hsl() #getting your coordinates
     if c:
         lat, lon, city = c
 #        hospital_loc(lat, lon)
